837ff3010f046ebc3442bc8e643837c09c8ed963
Andres Tobio <92232669+AndrewUru@users.noreply.github.com>
2025-12-30 20:20:34 +0100

    feat: Actualizar la URL del widget en el componente AIWidget

--- components/AIWidget.tsx
+++ components/AIWidget.tsx
@@ -1,12 +1,11 @@
 // components/AIWidget.tsx
 "use client";
-
 import Script from "next/script";
 
 export default function AIWidget() {
   return (
     <Script
-      src="https://agentes.elsaltoweb.es/api/widget?key=agt_v41zuu0e7mamgjm3gaq&accent=e2e4e9&brand=ElSaltoWeb&label=Chatea+con+nosotros&greeting=%C2%BFEn+qu%C3%A9+puedo+ayudarte+hoy%3F&position=right"
+      src="https://agentes.elsaltoweb.es/api/widget?key=agt_419oweh9oi6mjswq07p&accent=d324eb&brand=AI+Widget&label=Chatea+con+nosotros&greeting=%C2%BFEn+qu%C3%A9+puedo+ayudarte+hoy%3F&colorHeaderBg=008069&colorHeaderText=ffffff&colorChatBg=efe7dd&preview=1"
       strategy="afterInteractive"
       onError={() => console.error("[AI SaaS] Could not load the widget.")}
     />
