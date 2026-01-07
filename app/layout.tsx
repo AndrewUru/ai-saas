0f2ccda4a4a32d1ba7fa65d4001f0eda00c74078
Andres Tobio <92232669+AndrewUru@users.noreply.github.com>
2026-01-07 15:57:35 +0100

    refactor: Remove AIWidget component and integrate widget script directly into layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,6 @@ import type { Metadata } from "next";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import QueryProvider from "./query-provider";
-import AIWidget from "@/components/AIWidget";
-
 
 export const metadata: Metadata = {
   title: "AI SaaS",
@@ -25,7 +23,11 @@ export default function RootLayout({
           {children}
           <Footer />
         </QueryProvider>
-        <AIWidget />
+
+        <script
+          async
+          src="https://agentes.elsaltoweb.es/api/widget?key=agt_419oweh9oi6mjswq07p"
+        ></script>
       </body>
     </html>
   );
